a16f62928fd46220e2e699de2daf07b80b90b072
Jonathan Kammerland <jkammerland@gmail.com>
2024-11-18 23:15:23 +0100

    Added tags tests and refactor some suites

--- test/test_experiments.cpp
+++ test/test_experiments.cpp
@@ -14,8 +14,10 @@
  */
 
 #include "cbor_tags/cbor_concepts.h"
+#include "cbor_tags/cbor_detail.h"
 #include "cbor_tags/cbor_reflection.h"
 #include "doctest/doctest.h"
+#include "test_util.h"
 
 #include <algorithm>
 #include <cstddef>
@@ -23,13 +25,93 @@
 #include <fmt/core.h>
 #include <functional>
 #include <iostream>
+#include <list>
+#include <map>
 #include <memory>
 #include <memory_resource>
+#include <nameof.hpp>
 #include <ranges>
+#include <set>
 #include <sstream>
 #include <type_traits>
 #include <variant>
 #include <vector>
+
+using namespace cbor::tags;
+
+std::string some_data = "Hello world!";
+struct A {
+    std::int64_t a;
+    std::string  s;
+};
+
+// Helper function to print type and value
+template <typename T> constexpr void print_type_and_value(const T &value) {
+    if constexpr (std::is_same_v<T, A>) {
+        fmt::print("Got type <A>: with values a={}, s={}\n", value.a, value.s);
+    } else if constexpr (fmt::is_formattable<T>()) {
+        fmt::print("Got type <{}> with value <{}>\n", nameof::nameof_short_type<T>(), value);
+    } else {
+        fmt::print("Got type <{}>\n", nameof::nameof_short_type<T>());
+    }
+}
+
+TEST_CASE("Basic reflection") {
+    struct M {
+        int                a;
+        std::optional<int> b;
+    };
+
+    auto count = detail::aggregate_binding_count<M>;
+    fmt::print("M has {} members\n", count);
+    CHECK_EQ(count, 2);
+
+    auto is_constructible = IsBracesContructible<M, any, any>;
+    fmt::print("M is braces construct with 2 members: {}\n", is_constructible);
+    CHECK(is_constructible);
+
+    // Check if we can construct M with any and any
+    [[maybe_unused]] auto tmp1 = M{any{}, std::optional<int>{any{}}}; // This should compile
+
+    [[maybe_unused]] auto tmp2 = M{any{}, any{}}; // Question is if if we can compile this
+}
+
+TEST_CASE("Advanced reflection") {
+    struct Z {
+        int                        a;
+        float                      b;
+        std::string                c;
+        std::vector<int>           d;
+        std::map<std::string, int> e;
+        std::deque<double>         f;
+
+        A g;
+
+        std::optional<int>            h;
+        std::optional<std::list<int>> i;
+        std::vector<std::vector<int>> j;
+        std::multimap<int, int>       k;
+        std::set<std::pair<int, int>> l;
+    };
+
+    auto z = Z{42,
+               3.14f,
+               "Hello world!",
+               {1, 2, 3},
+               {{"one", 1}, {"two", 2}, {"three", 3}},
+               {1.0, 2.0, 3.0},
+               A{42, "Hello world!"},
+               std::nullopt,
+               std::list<int>{1, 2, 3},           // std::optional<std::list<int>>
+               {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, // std::vector<std::vector<int>>
+               {{1, 2}, {1, 2}, {1, 2}},          // std::multimap<int, int>
+               {{1, 2}, {3, 4}, {5, 6}}};         // std::set<std::pair<int, int>>
+
+    auto &&tuple = to_tuple(z);
+
+    std::apply([](auto &&...args) { (print_type_and_value(args), ...); }, tuple);
+    CHECK_EQ(detail::aggregate_binding_count<Z>, 12);
+}
 
 struct featureA {
     constexpr int decode(double) { return 1; }
