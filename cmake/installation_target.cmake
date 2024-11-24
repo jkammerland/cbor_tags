abccb516234e4891c52596f55a22d31c82c565ca
Jonathan Kammerland <jkammerland@gmail.com>
2024-11-24 19:16:49 +0100

    Cleanup reflection module generator

--- cmake/installation_target.cmake
+++ cmake/installation_target.cmake
@@ -27,6 +27,9 @@ write_basic_package_version_file(
   VERSION ${PROJECT_VERSION}
   COMPATIBILITY SameMajorVersion)
 
+# Configure the dependencies string for config file
+string(JOIN "\n" CBOR_TAGS_PUBLIC_DEPENDENCIES ${CBOR_TAGS_PUBLIC_DEPENDENCIES})
+
 # Generate the config file
 configure_package_config_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/cbor_tags-config.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/cbor_tags-config.cmake"
                               INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/cbor_tags)
