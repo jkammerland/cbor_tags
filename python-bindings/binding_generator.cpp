bcf36d774f74fc9a54371a634921497c392f387a
Jonathan Kammerland <jkammerland@gmail.com>
2024-11-27 19:40:01 +0100

    Working generator!

--- python-bindings/binding_generator.cpp
+++ python-bindings/binding_generator.cpp
@@ -1,4 +1,3 @@
-// #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/Frontend/CompilerInstance.h>
@@ -10,48 +9,88 @@
 #include <llvm/Support/CommandLine.h>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 struct StructInfo {
     std::string                                      name;
-    std::vector<std::pair<std::string, std::string>> members; // (type, name)
+    std::vector<std::pair<std::string, std::string>> members;
 };
+
+struct FunctionInfo {
+    std::string                                      name;
+    std::string                                      returnType;
+    std::vector<std::pair<std::string, std::string>> parameters;
+};
+
+using VisitCompleteCallback = std::function<void(std::vector<StructInfo> &&, std::vector<FunctionInfo> &&)>;
 
 class StructVisitor : public clang::RecursiveASTVisitor<StructVisitor> {
   public:
-    explicit StructVisitor(clang::ASTContext *context) : context_(context) {}
+    explicit StructVisitor(clang::ASTContext *context, VisitCompleteCallback cb) : context_(context), cb_(std::move(cb)) {}
 
     bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
+        // Check if it is a user-defined struct in the 'test' namespace
         if (declaration->isStruct() && !declaration->isImplicit()) {
-            StructInfo info;
-            info.name = declaration->getNameAsString();
-
-            // Collect member variables
-            for (const auto *field : declaration->fields()) {
-                std::string type = field->getType().getAsString();
-                std::string name = field->getNameAsString();
-                info.members.emplace_back(type, name);
+            const clang::DeclContext *declContext = declaration->getDeclContext();
+            if (const auto *namespaceDecl = llvm::dyn_cast<clang::NamespaceDecl>(declContext)) {
+                if (namespaceDecl->getName() == "test") { // Only process structs in the 'test' namespace
+                    llvm::outs() << "Struct: " << declaration->getName() << "\n";
+                    StructInfo info;
+                    info.name = declaration->getNameAsString();
+                    for (const auto *field : declaration->fields()) {
+                        std::string type = field->getType().getAsString();
+                        std::string name = field->getNameAsString();
+                        info.members.emplace_back(type, name);
+                    }
+                    structs_.push_back(info);
+                }
             }
-
-            structs_.push_back(info);
         }
         return true;
     }
 
-    const std::vector<StructInfo> &getStructs() const { return structs_; }
+    bool VisitFunctionDecl(clang::FunctionDecl *declaration) {
+        // Only capture functions that are user-defined and in the 'test' namespace
+        if (declaration->isFunctionOrMethod() && !declaration->isImplicit()) {
+            const clang::DeclContext *declContext = declaration->getDeclContext();
+            if (const auto *namespaceDecl = llvm::dyn_cast<clang::NamespaceDecl>(declContext)) {
+                if (namespaceDecl->getName() == "test") { // Only process functions in the 'test' namespace
+                    llvm::outs() << "Function: " << declaration->getName() << "\n";
+                    FunctionInfo info;
+                    info.name       = declaration->getNameAsString();
+                    info.returnType = declaration->getReturnType().getAsString();
+
+                    for (const auto *param : declaration->parameters()) {
+                        std::string type = param->getType().getAsString();
+                        std::string name = param->getNameAsString();
+                        info.parameters.emplace_back(type, name);
+                    }
+                    functions_.push_back(info);
+                }
+            }
+        }
+        return true;
+    }
+
+    ~StructVisitor() {
+        cb_(std::move(structs_), std::move(functions_));
+        llvm::outs() << "Destroying StructVisitor\n";
+    }
 
   private:
-    clang::ASTContext      *context_;
-    std::vector<StructInfo> structs_;
+    [[maybe_unused]] clang::ASTContext *context_;
+    VisitCompleteCallback               cb_;
+
+    std::vector<StructInfo>   structs_;
+    std::vector<FunctionInfo> functions_;
 };
 
 class StructASTConsumer : public clang::ASTConsumer {
   public:
-    explicit StructASTConsumer(clang::ASTContext *context) : visitor_(context) {}
+    explicit StructASTConsumer(clang::ASTContext *context, VisitCompleteCallback cb) : visitor_(context, cb) {}
 
     void HandleTranslationUnit(clang::ASTContext &context) override { visitor_.TraverseDecl(context.getTranslationUnitDecl()); }
-
-    const std::vector<StructInfo> &getStructs() const { return visitor_.getStructs(); }
 
   private:
     StructVisitor visitor_;
@@ -59,66 +98,116 @@ class StructASTConsumer : public clang::ASTConsumer {
 
 class BindingGeneratorAction : public clang::ASTFrontendAction {
   public:
+    explicit BindingGeneratorAction(VisitCompleteCallback cb) : cb_(std::move(cb)) {}
+
     std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &compiler, llvm::StringRef file) override {
-        return std::make_unique<StructASTConsumer>(&compiler.getASTContext());
+        fmt::print("Creating processing action for file: {}\n", file.str());
+        consumer_ = new StructASTConsumer(&compiler.getASTContext(), cb_);
+        return std::unique_ptr<clang::ASTConsumer>(consumer_);
     }
+
+    ~BindingGeneratorAction() override { llvm::outs() << "Destroying BindingGeneratorAction\n"; }
+
+  private:
+    VisitCompleteCallback cb_;
+    StructASTConsumer    *consumer_{};
 };
 
-void generateBindings(const std::vector<StructInfo> &structs, const std::string &moduleName) {
+void generateBindings(const std::vector<StructInfo> &structs, const std::vector<FunctionInfo> &functions, const std::string &moduleName) {
+
     std::ofstream out(moduleName + "_bindings.cpp");
 
-    // Write header
-    out << R"(#include <pybind11/pybind11.h>
-namespace py = pybind11;
+    // Write headers
+    out << "#include <pybind11/pybind11.h>\n"
+        << "#include <pybind11/stl.h>\n"
+        << "namespace py = pybind11;\n\n";
 
-)" << std::endl;
-
-    // Include all necessary headers
-    // Note: You might need to modify this part based on your actual header locations
+    // Include headers for structs and functions
     for (const auto &structInfo : structs) {
         out << fmt::format("#include \"{}.h\"\n", structInfo.name);
     }
 
     out << "\nPYBIND11_MODULE(" << moduleName << ", m) {\n";
 
-    // Generate bindings for each struct
+    // Generate struct bindings
     for (const auto &structInfo : structs) {
-        out << fmt::format("    py::class_<{0}>(m, \"{0}\")\n", structInfo.name);
-        out << "        .def(py::init<>())  // Default constructor\n";
+        out << fmt::format("    py::class_<{0}>(m, \"{0}\")\n", structInfo.name) << "        .def(py::init<>())\n";
 
-        // Add members
         for (const auto &[type, name] : structInfo.members) {
             out << fmt::format("        .def_readwrite(\"{0}\", &{1}::{0})\n", name, structInfo.name);
         }
         out << "        ;\n\n";
     }
 
+    // Generate function bindings
+    for (const auto &funcInfo : functions) {
+        out << fmt::format("    m.def(\"{0}\", &{0});\n", funcInfo.name);
+    }
+
     out << "}\n";
 }
 
 int main(int argc, const char **argv) {
-    if (argc < 3) {
-        llvm::errs() << "Usage: " << argv[0] << " <module-name> <source-file> [compiler-args...]\n";
+    if (argc < 2) {
+        llvm::errs() << "Usage: " << argv[0] << "<source-file>... [compiler-args...]\n";
         return 1;
     }
 
-    std::string moduleName = argv[1];
+    // Print all args
+    for (int i = 0; i < argc; ++i) {
+        llvm::outs() << "argv[" << i << "] = " << argv[i] << "\n";
+    }
 
-    // Setup Clang tool
+    std::string moduleName = "my_module";
+
     auto expectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, llvm::cl::getGeneralCategory());
     if (!expectedParser) {
-        llvm::errs() << "Error parsing command line arguments\n";
+        llvm::errs() << "Error parsing command line arguments: " << expectedParser.takeError() << "\n";
         return 1;
     }
 
     clang::tooling::ClangTool tool(expectedParser->getCompilations(), expectedParser->getSourcePathList());
 
-    // Run the Clang tool
-    std::vector<StructInfo> structs;
-    tool.run(clang::tooling::newFrontendActionFactory<BindingGeneratorAction>().get());
+    // Create an action factory
+    class BindingGeneratorActionFactory : public clang::tooling::FrontendActionFactory {
+      public:
+        explicit BindingGeneratorActionFactory(VisitCompleteCallback cb) : cb_(std::move(cb)) {}
 
-    // Generate bindings
-    generateBindings(structs, moduleName);
+        std::unique_ptr<clang::FrontendAction> create() override {
+            action_ = new BindingGeneratorAction(cb_);
+            return std::unique_ptr<clang::FrontendAction>(action_);
+        }
+
+        BindingGeneratorAction *getAction() const { return action_; }
+
+      private:
+        VisitCompleteCallback   cb_;
+        BindingGeneratorAction *action_;
+    };
+
+    std::vector<StructInfo>   structs;
+    std::vector<FunctionInfo> functions;
+
+    auto cb = [&structs, &functions](std::vector<StructInfo> &&structs_, std::vector<FunctionInfo> &&functions_) {
+        structs   = std::move(structs_);
+        functions = std::move(functions_);
+    };
+
+    auto factoryPtr = std::make_unique<BindingGeneratorActionFactory>(cb);
+
+    if (tool.run(factoryPtr.get()) != 0) {
+        llvm::errs() << "Error running tool\n";
+        return 1;
+    }
+
+    for (const auto &structInfo : structs) {
+        llvm::outs() << "Struct: " << structInfo.name << "\n";
+        for (const auto &[type, name] : structInfo.members) {
+            llvm::outs() << "    " << type << " " << name << "\n";
+        }
+    }
+
+    generateBindings(structs, functions, moduleName);
 
     return 0;
 }
---
+++ python-bindings/readme.md
@@ -0,0 +1,23 @@
+binding_generator
+argv[1] = struct_example_impl.cpp
+argv[2] = --
+argv[3] = -std=c++14
+argv[4] = -I/usr/lib/gcc/x86_64-redhat-linux/14/include
+argv[5] = -I/usr/include/c++/14
+argv[6] = -I/usr/include/c++/14/x86_64-redhat-linux
+argv[7] = -I/usr/include/c++/14/backward
+argv[8] = -I/usr/local/include
+argv[9] = -I/usr/include
+Creating processing action for file: ...../struct_example_impl.cpp
+Struct: A
+Function: processA
+Struct: B
+Function: processB
+Destroying StructVisitor
+Destroying BindingGeneratorAction
+Struct: A
+    int a1
+    double a2
+    std::string a3
+Struct: B
+    std::string b
