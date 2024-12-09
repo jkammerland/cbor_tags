9fff5606da5aa7eb1c2ee53e5977c85bfb393930
Jonathan Kammerland <jkammerland@gmail.com>
2024-12-09 22:50:55 +0100

    Removed redundant py-gen. Moved to new repo

--- python-bindings/tests/another_ex.hpp
+++
@@ -1,9 +0,0 @@
-#include <complex>
-#include <string>
-
-struct alpha {
-    std::complex<double> a;
-    std::string          b;
-
-    std::string print(int) const { return "alpha" + std::to_string(a.real()) + std::to_string(a.imag()) + b; }
-};
