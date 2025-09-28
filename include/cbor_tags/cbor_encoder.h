a88da8be86a7f369f4cf6e010793d9335321c8a6
Jonathan Kammerland <jkammerland@gmail.com>
2025-09-28 14:19:25 +0200

    Complete mixin encoder refactor

--- include/cbor_tags/cbor_encoder.h
+++ include/cbor_tags/cbor_encoder.h
@@ -24,6 +24,16 @@
 namespace cbor::tags {
 
 template <typename T> struct cbor_header_encoder;
+template <typename T> struct cbor_integer_encoder;
+template <typename T> struct cbor_tag_encoder;
+template <typename T> struct cbor_string_encoder;
+template <typename T> struct cbor_array_encoder;
+template <typename T> struct cbor_map_encoder;
+template <typename T> struct cbor_tuple_encoder;
+template <typename T> struct cbor_aggregate_encoder;
+template <typename T> struct cbor_class_encoder;
+template <typename T> struct cbor_float_encoder;
+template <typename T> struct cbor_simple_encoder;
 
 template <typename OutputBuffer, IsOptions Options, template <typename> typename... Encoders>
     requires ValidCborBuffer<OutputBuffer>
@@ -71,162 +81,212 @@ struct encoder : Encoders<encoder<OutputBuffer, Options, Encoders...>>... {
         }
     }
 
-    template <IsUnsigned T> constexpr void encode(T value) { encode_major_and_size(value, static_cast<byte_type>(0x00)); }
+    // Variadic friends only in c++26, must be public
+    detail::appender<OutputBuffer> appender_;
+    OutputBuffer                  &data_;
+};
 
-    template <IsSigned T> constexpr void encode(T value) {
+template <typename T> struct cbor_integer_encoder {
+    template <IsUnsigned U> constexpr void encode(U value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value, static_cast<typename T::byte_type>(0x00));
+    }
+
+    template <IsSigned U> constexpr void encode(U value) {
+        auto &self = detail::underlying<T>(this);
         if (value >= 0) {
-            encode_major_and_size(static_cast<std::uint64_t>(value), static_cast<byte_type>(0x00));
+            self.encode_major_and_size(static_cast<std::uint64_t>(value), static_cast<typename T::byte_type>(0x00));
         } else {
-            encode_major_and_size(static_cast<std::uint64_t>(-1 - value), static_cast<byte_type>(0x20));
+            self.encode_major_and_size(static_cast<std::uint64_t>(-1 - value), static_cast<typename T::byte_type>(0x20));
         }
     }
 
     constexpr void encode(negative value) {
-        encode_major_and_size(static_cast<std::uint64_t>(value.value - 1), static_cast<byte_type>(0x20));
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(static_cast<std::uint64_t>(value.value - 1), static_cast<typename T::byte_type>(0x20));
     }
 
     constexpr void encode(integer value) {
+        auto &self = detail::underlying<T>(this);
         if (value.is_negative) {
-            encode(negative{value.value});
+            self.encode(negative{value.value});
         } else {
-            encode(value.value);
+            self.encode(value.value);
         }
     }
 
-    template <std::uint64_t N> constexpr void encode(static_tag<N>) { encode_major_and_size(N, static_cast<byte_type>(0xC0)); }
-    template <IsUnsigned T> constexpr void    encode(dynamic_tag<T> value) {
-        encode_major_and_size(value.cbor_tag, static_cast<byte_type>(0xC0));
+    template <IsEnum U> constexpr void encode(U value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode(static_cast<std::underlying_type_t<U>>(value));
+    }
+};
+
+template <typename T> struct cbor_tag_encoder {
+    template <std::uint64_t N> constexpr void encode(static_tag<N>) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(N, static_cast<typename T::byte_type>(0xC0));
     }
 
-    template <IsString T> constexpr void encode(const T &value) {
-        encode_major_and_size(value.size(), static_cast<byte_type>(get_major_3_bit_tag<T>()));
-        appender_(data_, value);
+    template <IsUnsigned U> constexpr void encode(dynamic_tag<U> value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.cbor_tag, static_cast<typename T::byte_type>(0xC0));
     }
 
-    template <IsArray T> constexpr void encode(const T &value) {
-        encode_major_and_size(value.size(), static_cast<byte_type>(0x80));
+    template <IsTaggedTuple Tuple> constexpr void encode(const Tuple &value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.first, static_cast<typename T::byte_type>(0xC0));
+        self.encode(value.second);
+    }
+};
+
+template <typename T> struct cbor_string_encoder {
+    template <IsString String> constexpr void encode(const String &value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.size(), static_cast<typename T::byte_type>(get_major_3_bit_tag<String>()));
+        self.appender_(self.data_, value);
+    }
+};
+
+template <typename T> struct cbor_array_encoder {
+    template <IsArray Array> constexpr void encode(const Array &value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.size(), static_cast<typename T::byte_type>(0x80));
         for (const auto &item : value) {
-            encode(item);
+            self.encode(item);
         }
     }
+};
 
-    template <IsMap T> constexpr void encode(const T &value) {
-        encode_major_and_size(value.size(), static_cast<byte_type>(0xA0));
+template <typename T> struct cbor_map_encoder {
+    template <IsMap Map> constexpr void encode(const Map &value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.size(), static_cast<typename T::byte_type>(0xA0));
         for (const auto &[key, mapped_value] : value) {
-            encode(key);
-            encode(mapped_value);
+            self.encode(key);
+            self.encode(mapped_value);
         }
     }
+};
 
-    template <IsTaggedTuple T> constexpr void encode(const T &value) {
-        /* Tag being first already garanteed by the concept */
-        encode_major_and_size(value.first, static_cast<byte_type>(0xC0));
-        encode(value.second);
+template <typename T> struct cbor_aggregate_encoder {
+    template <typename Tuple> constexpr void aggregate_encode(Tuple &&tuple) {
+        auto &self = detail::underlying<T>(this);
+        std::apply(
+            [&self](const auto &...args) {
+                constexpr auto size_ = sizeof...(args);
+                if constexpr (size_ > 1 && T::options::wrap_groups) {
+                    self.encode(as_array{size_});
+                }
+                (self.encode(args), ...);
+            },
+            std::forward<Tuple>(tuple));
     }
 
-    template <typename T>
-        requires(IsAggregate<T> && !IsClassWithEncodingOverload<self_t, T>)
-    constexpr void encode(const T &value) {
-        if constexpr (HasInlineTag<T>) {
+    template <typename Value>
+        requires(IsAggregate<Value> && !IsClassWithEncodingOverload<T, Value>)
+    constexpr void encode(const Value &value) {
+        auto &self = detail::underlying<T>(this);
+        if constexpr (HasInlineTag<Value>) {
             const auto &&tuple = to_tuple(value);
-            encode_major_and_size(T::cbor_tag, static_cast<byte_type>(0xC0));
+            self.encode_major_and_size(Value::cbor_tag, static_cast<typename T::byte_type>(0xC0));
             std::apply(
-                [this](const auto &...args) {
+                [&self](const auto &...args) {
                     constexpr auto   size_        = sizeof...(args);
-                    constexpr size_t has_tag_or_1 = std::conditional_t < HasStaticTag<T> || HasDynamicTag<T>,
+                    constexpr size_t has_tag_or_1 = std::conditional_t < HasStaticTag<Value> || HasDynamicTag<Value>,
                                      std::integral_constant<size_t, 2>, std::integral_constant < size_t, 1 >> ::value;
-                    if constexpr (size_ > has_tag_or_1 && Options::wrap_groups) {
-                        this->encode(as_array{size_});
+                    if constexpr (size_ > has_tag_or_1 && T::options::wrap_groups) {
+                        self.encode(as_array{size_});
                     }
-                    (this->encode(args), ...);
+                    (self.encode(args), ...);
                 },
                 tuple);
-        } else if constexpr (IsTag<T>) {
+        } else if constexpr (IsTag<Value>) {
             const auto &&tuple = to_tuple(value);
             static_assert(IsTag<std::remove_cvref_t<decltype(std::get<0>(tuple))>>, "Tag must be first element of tuple");
-            encode_major_and_size(std::get<0>(tuple), static_cast<byte_type>(0xC0));
-            aggregate_encode(detail::tuple_tail(tuple));
+            self.encode_major_and_size(std::get<0>(tuple), static_cast<typename T::byte_type>(0xC0));
+            self.aggregate_encode(detail::tuple_tail(tuple));
         } else {
             const auto &&tuple = to_tuple(value);
-            aggregate_encode(std::forward<decltype(tuple)>(tuple));
+            self.aggregate_encode(std::forward<decltype(tuple)>(tuple));
         }
     }
+};
 
-    template <IsUntaggedTuple T> constexpr void encode(const T &value) { aggregate_encode(value); }
+template <typename T> struct cbor_tuple_encoder {
+    template <IsUntaggedTuple Tuple> constexpr void encode(const Tuple &value) { detail::underlying<T>(this).aggregate_encode(value); }
+};
 
+template <typename T> struct cbor_class_encoder {
     template <typename C>
-        requires(IsClassWithEncodingOverload<self_t, C>)
+        requires(IsClassWithEncodingOverload<T, C>)
     constexpr void encode(const C &value) {
-        constexpr bool has_transcode      = HasTranscodeMethod<self_t, C>;
-        constexpr bool has_encode         = HasEncodeMethod<self_t, C>;
-        constexpr bool has_free_encode    = HasEncodeFreeFunction<self_t, C>;
-        constexpr bool has_free_transcode = HasTranscodeFreeFunction<self_t, C>;
+        auto          &self               = detail::underlying<T>(this);
+        constexpr bool has_transcode      = HasTranscodeMethod<T, C>;
+        constexpr bool has_encode         = HasEncodeMethod<T, C>;
+        constexpr bool has_free_encode    = HasEncodeFreeFunction<T, C>;
+        constexpr bool has_free_transcode = HasTranscodeFreeFunction<T, C>;
         static_assert(
             has_transcode ^ has_encode ^ has_free_encode ^ has_free_transcode,
             "Class must have either (const) transcode or encode method, also do not forget to return value from the transcoding operation! "
             "Give friend access if members are private, i.e friend cbor::tags::Access (full namespace is required)");
 
-        // Automatic tag encoding
         if constexpr (IsClassWithTagOverload<C>) {
-            this->encode(detail::get_major_6_tag_from_class(value));
+            self.encode(detail::get_major_6_tag_from_class(value));
         }
 
-        // For now, the only errors from encoding are exceptions. It will be caught by the operator(...) function, up top
         if constexpr (has_transcode) {
-            [[maybe_unused]] auto result = Access::transcode(*this, value);
+            [[maybe_unused]] auto result = Access::transcode(self, value);
         } else if constexpr (has_encode) {
-            [[maybe_unused]] auto result = Access::encode(*this, value);
+            [[maybe_unused]] auto result = Access::encode(self, value);
         } else if constexpr (has_free_encode) {
-            /* This requires an indirect call in order for some compilers to find the overload. */
-            [[maybe_unused]] auto result = detail::adl_indirect_encode(*this, value);
+            [[maybe_unused]] auto result = detail::adl_indirect_encode(self, value);
         } else if constexpr (has_free_transcode) {
-            /* Transcode does not require an indirect call, because no other methods exist with the same name (encode)*/
-            [[maybe_unused]] auto result = transcode(*this, value);
+            [[maybe_unused]] auto result = transcode(self, value);
         }
     }
+};
 
-    template <IsEnum T> constexpr void encode(T value) { this->encode(static_cast<std::underlying_type_t<T>>(value)); }
-
+template <typename T> struct cbor_float_encoder {
     constexpr void encode(float16_t value) {
-        appender_.multi_append(data_, static_cast<byte_type>(0xF9), static_cast<byte_type>(value.value >> 8),
-                               static_cast<byte_type>(value.value & 0xFF));
+        auto &self = detail::underlying<T>(this);
+        self.appender_.multi_append(self.data_, static_cast<typename T::byte_type>(0xF9),
+                                    static_cast<typename T::byte_type>(value.value >> 8),
+                                    static_cast<typename T::byte_type>(value.value & 0xFF));
     }
 
     constexpr void encode(float value) {
+        auto      &self = detail::underlying<T>(this);
         const auto bits = std::bit_cast<std::uint32_t>(value);
-        appender_.multi_append(data_, static_cast<byte_type>(0xFA), static_cast<byte_type>(bits >> 24), static_cast<byte_type>(bits >> 16),
-                               static_cast<byte_type>(bits >> 8), static_cast<byte_type>(bits));
+        self.appender_.multi_append(self.data_, static_cast<typename T::byte_type>(0xFA), static_cast<typename T::byte_type>(bits >> 24),
+                                    static_cast<typename T::byte_type>(bits >> 16), static_cast<typename T::byte_type>(bits >> 8),
+                                    static_cast<typename T::byte_type>(bits));
     }
 
     constexpr void encode(double value) {
+        auto      &self = detail::underlying<T>(this);
         const auto bits = std::bit_cast<std::uint64_t>(value);
-        appender_.multi_append(data_, static_cast<byte_type>(0xFB), static_cast<byte_type>(bits >> 56), static_cast<byte_type>(bits >> 48),
-                               static_cast<byte_type>(bits >> 40), static_cast<byte_type>(bits >> 32), static_cast<byte_type>(bits >> 24),
-                               static_cast<byte_type>(bits >> 16), static_cast<byte_type>(bits >> 8), static_cast<byte_type>(bits));
+        self.appender_.multi_append(self.data_, static_cast<typename T::byte_type>(0xFB), static_cast<typename T::byte_type>(bits >> 56),
+                                    static_cast<typename T::byte_type>(bits >> 48), static_cast<typename T::byte_type>(bits >> 40),
+                                    static_cast<typename T::byte_type>(bits >> 32), static_cast<typename T::byte_type>(bits >> 24),
+                                    static_cast<typename T::byte_type>(bits >> 16), static_cast<typename T::byte_type>(bits >> 8),
+                                    static_cast<typename T::byte_type>(bits));
+    }
+};
+
+template <typename T> struct cbor_simple_encoder {
+    constexpr void encode(bool value) {
+        auto &self = detail::underlying<T>(this);
+        self.appender_(self.data_, value ? static_cast<typename T::byte_type>(0xF5) : static_cast<typename T::byte_type>(0xF4));
     }
 
-    constexpr void encode(bool value) { appender_(data_, value ? static_cast<byte_type>(0xF5) : static_cast<byte_type>(0xF4)); }
+    constexpr void encode(std::nullptr_t) {
+        auto &self = detail::underlying<T>(this);
+        self.appender_(self.data_, static_cast<typename T::byte_type>(0xF6));
+    }
 
-    constexpr void encode(std::nullptr_t) { appender_(data_, static_cast<byte_type>(0xF6)); }
-
-    constexpr void encode(simple value) { encode_major_and_size(value.value, static_cast<byte_type>(0xE0)); }
-
-    // Variadic friends only in c++26, must be public
-    detail::appender<OutputBuffer> appender_;
-    OutputBuffer                  &data_;
-
-  private:
-    // Helper method to avoid code duplication
-    template <typename Tuple> void aggregate_encode(Tuple &&tuple) {
-        std::apply(
-            [this](const auto &...args) {
-                constexpr auto size_ = sizeof...(args);
-                if constexpr (size_ > 1 && Options::wrap_groups) {
-                    this->encode(as_array{size_});
-                }
-                (this->encode(args), ...);
-            },
-            std::forward<Tuple>(tuple));
+    constexpr void encode(simple value) {
+        auto &self = detail::underlying<T>(this);
+        self.encode_major_and_size(value.value, static_cast<typename T::byte_type>(0xE0));
     }
 };
 
@@ -262,6 +322,7 @@ template <typename T> struct cbor_header_encoder {
 
 template <typename OutputBuffer> inline auto make_encoder(OutputBuffer &buffer) {
     return encoder<OutputBuffer, Options<default_expected, default_wrapping>, cbor_header_encoder, cbor_optional_encoder,
-                   cbor_variant_encoder>(buffer);
+                   cbor_variant_encoder, cbor_tag_encoder, cbor_integer_encoder, cbor_string_encoder, cbor_array_encoder, cbor_map_encoder,
+                   cbor_aggregate_encoder, cbor_tuple_encoder, cbor_class_encoder, cbor_float_encoder, cbor_simple_encoder>(buffer);
 }
 } // namespace cbor::tags
