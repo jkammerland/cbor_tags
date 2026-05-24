6325fd5212443d9e54cc8334447c30a5a4338b05
Jonathan Kammerland <jkammerland@gmail.com>
2026-05-24 12:49:06 +0200

    test(smart-ptr): cover split-header aggregate graph

--- test/test_header_split_smart_ptr.cpp
+++ test/test_header_split_smart_ptr.cpp
@@ -33,3 +33,27 @@ TEST_CASE("shared graph split header is directly usable") {
     REQUIRE(static_cast<bool>(decoded));
     CHECK_EQ(*decoded, 42);
 }
+
+struct TestGraphStruct {
+    int                 x;
+    std::vector<double> ys;
+};
+
+TEST_CASE("shared graph split header roundtrips aggregate payloads") {
+    std::vector<std::byte>    encoded;
+    const std::vector<double> expected_ys{1.0, 2.0};
+
+    auto value = std::make_shared<TestGraphStruct>(TestGraphStruct{.x = 1, .ys = expected_ys});
+    auto enc   = cbor::tags::make_encoder<cbor::tags::ext::smart_ptr::shared_graph_codec>(encoded);
+    cbor::tags::ext::smart_ptr::shared_graph_encode_session encode_graph;
+
+    REQUIRE(enc(cbor::tags::ext::smart_ptr::as_shared_graph(encode_graph, value)));
+
+    std::shared_ptr<TestGraphStruct>                        decoded;
+    cbor::tags::ext::smart_ptr::shared_graph_decode_session decode_graph;
+    auto dec = cbor::tags::make_decoder<cbor::tags::ext::smart_ptr::shared_graph_codec>(encoded);
+    REQUIRE(dec(cbor::tags::ext::smart_ptr::as_shared_graph(decode_graph, decoded)));
+    REQUIRE(static_cast<bool>(decoded));
+    CHECK_EQ(decoded->x, 1);
+    CHECK_EQ(decoded->ys, expected_ys);
+}
