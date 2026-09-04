722233b02ddeda6aa9a1242ce02bfc518b4ae1b0
jkammerland <71674599+jkammerland@users.noreply.github.com>
2026-09-04 06:07:07 +0200

    chore(release): prepare v0.24.1 (#132)

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.files import copy
 
 class CborTagsConan(ConanFile):
     name = "cbor-tags"
-    version = "0.24.0"
+    version = "0.24.1"
     license = "MIT"
     description = "Binary tagging library with automatic encoding/decoding for CBOR"
     homepage = "https://github.com/jkammerland/cbor_tags"
